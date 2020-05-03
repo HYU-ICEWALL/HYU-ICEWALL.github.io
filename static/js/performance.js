a4cc79c70371c27352acc9b1b258c6ffcca84416
1cekrim <1cekrim.dev@gmail.com>
2020-05-03 20:23:51 +0900

    실적 테이블 template 추가

--- static/js/performance.js
+++ static/js/performance.js
@@ -1,1 +1,17 @@
-console.log("performance");
+var t =
+    new Vue({
+        el: '#view_tables',
+        data: function () {
+            return {
+                tables: [{
+                    title: '동아리 수상 경력',
+                    columns: ['번호', '구분', '대회명', '수상내역', '수상년도'],
+                    data: [1, '대내', '제 2회 한양대학교 해킹방어대회(HCTF)', '1위', 2019]
+                }, {
+                    title: '동아리 활동 경력',
+                    columns: ['번호', '활동내역', '년도'],
+                    data: [1, '학내 홈페이지 상시 점검', '2019']
+                }]
+            }
+        }
+    })
